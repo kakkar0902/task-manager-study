80a5cbb9ef016a15e6b841d30398cac2486b185d
Sonia Kakkar <your_github_email_here>
2026-03-02 19:54:00 -0400

    Week 1 Python fundamentals completed

---
+++ week1_practice.py
@@ -0,0 +1,15 @@
+print("Week 1 Practice Running")
+
+for i in range(5):
+    print("Loop number:", i)
+
+def greet(name):
+    print("Hello", name)
+
+greet("Sonia")
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
